226df23d7bc8fe9e6be0d0f4e51a13887559ecaf
kovardin <artem.kovardin@gmail.com>
2022-03-12 10:41:25 +0300

    updated readme

--- android/src/main/kotlin/ru/kovardin/flutter_yandex_ads/views/YandexAdsNative.kt
+++ android/src/main/kotlin/ru/kovardin/flutter_yandex_ads/views/YandexAdsNative.kt
@@ -1,13 +1,9 @@
 package ru.kovardin.flutter_yandex_ads.views
 
 import android.content.Context
+import android.graphics.Color
 import android.view.View
-import com.yandex.mobile.ads.banner.AdSize
-import com.yandex.mobile.ads.banner.BannerAdEventListener
-import com.yandex.mobile.ads.banner.BannerAdView
-import com.yandex.mobile.ads.common.AdRequest
-import com.yandex.mobile.ads.common.AdRequestError
-import com.yandex.mobile.ads.common.ImpressionData
+import android.widget.TextView
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
@@ -19,47 +15,16 @@ class YandexAdsNative: PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 }
 
 class Native(context: Context, id: Int) : PlatformView {
-    //    private val textView: TextView
-    private val banner: BannerAdView
-
+    var textView: TextView
     init {
-
-        banner = BannerAdView(context);
-        banner.setAdSize(AdSize.BANNER_320x50)
-        banner.setAdUnitId("R-M-1582183-1")
-        banner.setBannerAdEventListener(object : BannerAdEventListener {
-            override fun onAdLoaded() {
-            }
-
-            override fun onAdFailedToLoad(error: AdRequestError) {
-            }
-
-            override fun onAdClicked() {
-            }
-
-            override fun onLeftApplication() {
-            }
-
-            override fun onReturnedToApplication() {
-            }
-
-            override fun onImpression(data: ImpressionData?) {
-            }
-        })
-
-
-        val request: AdRequest = AdRequest.Builder().build()
-        banner.loadAd(request)
-
-
-//        textView = TextView(context)
-//        textView.textSize = 14f
-//        textView.setBackgroundColor(Color.rgb(255, 255, 255))
-//        textView.text = "Rendered on a native Android view (id: $id)"
+        textView = TextView(context)
+        textView.textSize = 14f
+        textView.setBackgroundColor(Color.rgb(255, 255, 255))
+        textView.text = "Rendered on a native Android view (id: $id)"
     }
 
     override fun getView(): View {
-        return banner;
+        return textView;
     }
 
     override fun dispose() {}
